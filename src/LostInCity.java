6f3547dcec0bf614f7355605bb2adfdcf79e5a43
kunal-kumar-singh <kunal.kumar.singh.mnnit@gmail.com>
2020-04-28 19:29:48 +0530

    Adding HelloWorld Project new commit Dijkastra

---
+++ src/LostInCity.java
@@ -0,0 +1,48 @@
+//https://www.hackerearth.com/practice/algorithms/graphs/shortest-path-algorithms/practice-problems/algorithm/lost-in-city-f6e7f540/description/
+import java.util.*;
+public class LostInCity {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t-->0) {
+            int n = in.nextInt();
+            int m =  in.nextInt();
+            int s = in.nextInt()-1;
+            int[] sweet = new int[n];
+            int[] distance = new int[n];
+            Arrays.fill(distance, Integer.MAX_VALUE);
+            for(int i=0;i<n;i++) {
+                sweet[i] = in.nextInt();
+            }
+            ArrayList[] graph = new ArrayList[n+1];
+            for(int i=0;i<m;i++) {
+                int start = in.nextInt()-1;
+                int end = in.nextInt()-1;
+                if(Objects.isNull(graph[start])) {
+                    graph[start] = new ArrayList<Integer>();
+                }
+                graph[start].add(end);
+            }
+            Queue<Integer> p = new LinkedList<Integer>();
+            distance[s] = sweet[s];
+            p.add(s);
+            while(!p.isEmpty()) {
+                int temp = p.poll();
+                ArrayList<Integer> parent = graph[temp];
+                if(parent!=null) {
+                    for(int i=0;i<parent.size();i++) {
+                        int child = parent.get(i);
+                        int value = Math.max(distance[temp], sweet[child]);
+                        if(value<distance[child]) {
+                            distance[child] = value;
+                            p.add(child);
+                        }
+                    }
+                }
+            }
+            for(int i=0;i<n;i++)
+                System.out.print(distance[i]+" ");
+            System.out.println();
+        }
+    }
+}

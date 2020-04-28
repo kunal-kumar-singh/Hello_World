52432315463036a60d7b6d31a2ab0d1ffac9fff0
kunal-kumar-singh <kunal.kumar.singh.mnnit@gmail.com>
2020-04-28 08:25:25 +0530

    Adding HelloWorld Project new commit

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,6 +2,5 @@ public class MyClass {
         public static void main(String[] args) {
             System.out.println("Hello World!!");
             System.out.println("Hello World!!");
-
         }
 }

cdb600f639a795f1953d1370789182b1e56d317d
kunal-kumar-singh <kunal.kumar.singh.mnnit@gmail.com>
2020-04-28 20:09:49 +0530

    Adding HelloWorld Project new commit Dijkastra

--- src/LostInCity.java
+++ src/LostInCity.java
@@ -2,39 +2,39 @@
 import java.util.*;
 public class LostInCity {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        while(t-->0) {
-            int n = in.nextInt();
-            int m =  in.nextInt();
-            int s = in.nextInt()-1;
-            int[] sweet = new int[n];
-            int[] distance = new int[n];
-            Arrays.fill(distance, Integer.MAX_VALUE);
-            for(int i=0;i<n;i++) {
-                sweet[i] = in.nextInt();
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            int s=sc.nextInt()-1;
+            int[] sweet=new int[n];
+            int[] distance=new int[n];
+            Arrays.fill(distance,Integer.MAX_VALUE);
+            for(int i=0;i<n;i++){
+                sweet[i]=sc.nextInt();
             }
-            ArrayList[] graph = new ArrayList[n+1];
+            ArrayList[] graph=new ArrayList[n+1];
             for(int i=0;i<m;i++) {
-                int start = in.nextInt()-1;
-                int end = in.nextInt()-1;
+                int start=sc.nextInt()-1;
+                int end=sc.nextInt()-1;
                 if(Objects.isNull(graph[start])) {
                     graph[start] = new ArrayList<Integer>();
                 }
                 graph[start].add(end);
             }
-            Queue<Integer> p = new LinkedList<Integer>();
-            distance[s] = sweet[s];
+            Queue<Integer> p=new LinkedList<Integer>();
+            distance[s]=sweet[s];
             p.add(s);
             while(!p.isEmpty()) {
-                int temp = p.poll();
-                ArrayList<Integer> parent = graph[temp];
+                int temp=p.poll();
+                ArrayList<Integer> parent=graph[temp];
                 if(parent!=null) {
                     for(int i=0;i<parent.size();i++) {
-                        int child = parent.get(i);
-                        int value = Math.max(distance[temp], sweet[child]);
+                        int child=parent.get(i);
+                        int value=Math.max(distance[temp], sweet[child]);
                         if(value<distance[child]) {
-                            distance[child] = value;
+                            distance[child]=value;
                             p.add(child);
                         }
                     }
